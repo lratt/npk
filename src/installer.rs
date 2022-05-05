f364fee53db4767291e468aff640655bd2a3dc44
lratt <lratt@mailbox.org>
2022-05-05 11:01:12 +0200

    clone new repos only

--- src/installer.rs
+++ src/installer.rs
@@ -33,11 +33,17 @@ impl Installer {
         (repo, cfg): (&String, &PackageAuthorConfig),
         into: P,
     ) -> Result<()> {
+        let repo_path = into.as_ref().join(PKG_NAME).join("start").join(repo);
+        match git2::Repository::open(&repo_path) {
+            Err(e) if e.code() == git2::ErrorCode::NotFound => {}
+            Err(e) => return Err(e.into()),
+            Ok(_) => return Ok(()),
+        }
+
         let repo_url = cfg
             .repo
             .clone()
             .unwrap_or_else(|| format!("https://github.com/{}/{}", author, repo));
-        let repo_path = into.as_ref().join(PKG_NAME).join("start").join(repo);
 
         println!("Cloning {}", &repo_url);
         git2::build::RepoBuilder::new()
