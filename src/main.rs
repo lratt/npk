fd7e8ce6462661b65eb427066844e25a43b9aae3
lratt <lratt@mailbox.org>
2022-05-07 13:03:08 +0200

    use arg 0 instead of cargo_pkg_name in usage

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,7 @@ mod installer;
 
 pub const PKG_NAME: &str = env!("CARGO_PKG_NAME");
 
-fn print_usage() {
+fn print_usage(cmd: &str) {
     println!(
         "USAGE:
     {} [command]
@@ -21,14 +21,14 @@ COMMANDS:
     upgrade, u     updates current packages
     help, h       prints this message
 ",
-        PKG_NAME
+        cmd
     );
 }
 
 fn main() -> anyhow::Result<()> {
     let args = std::env::args().collect::<Vec<_>>();
     if args.len() < 2 {
-        print_usage();
+        print_usage(&args[0]);
         return Ok(());
     }
     let cmd = &args[1];
@@ -39,10 +39,10 @@ fn main() -> anyhow::Result<()> {
     match &cmd[..] {
         "i" | "install" => installer.all_repos(Installer::clone_repo)?,
         "u" | "upgrade" => installer.all_repos(Installer::pull_repo)?,
-        "h" | "help" => print_usage(),
+        "h" | "help" => print_usage(&args[0]),
         _ => {
             println!("invalid command");
-            print_usage();
+            print_usage(&args[0]);
         }
     }
 
